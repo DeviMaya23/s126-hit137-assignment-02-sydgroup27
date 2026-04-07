cbb77dbeba208353cdd14bb254b328d0632d694f
devi <utilities@evimay.mailer.me>
2026-04-08 00:40:50 +1000

    feat: add docstring for scanner.py

--- Question 2/scanner.py
+++ Question 2/scanner.py
@@ -1,6 +1,12 @@
-def tokenise():
+def tokenise(input_line:str) -> list[tuple[str, str]]:
     """
     Scans the input data and returns a list of tokens.
+    Args:
+    input_line: a string of input line, coming from input file.
+
+    Return:
+    [(token_type, token_value), ...]
+    If it's an empty line, return None.
     """
     # Implementation of the scanner goes here
     pass
