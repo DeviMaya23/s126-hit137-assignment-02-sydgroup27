333d921424e741de4c305deeb9f7709e291cd896
devi <utilities@evimay.mailer.me>
2026-04-07 13:42:25 +1000

    feat: (q2) init factor function to parse unary negative

--- Question 2/math_parser.py
+++ Question 2/math_parser.py
@@ -1,6 +1,34 @@
+def expression(token_list:list[tuple[str, str]], index:int):
+    """
+    Returns an expression value.
+    """
+    result = factor(token_list, index)
+
+    return result
+
+def factor(token_list:list[tuple[str, str]], index:int):
+    """
+    Returns a factor value.
+    """
+    token = token_list[index]
+
+    if token[0] == "OP" and token[1] == "-":
+        return -factor(token_list, index + 1)
+
+    if token[0] == "NUM":
+        return float(token[1])
+    
+    return None
+
+
 def parse():
     """
     Parses the input data and returns tree and result.
     """
-    # Implementation of the parser goes here
-    pass
+
+    dummy = [("OP", "-"),("OP", "-"),("NUM", "3"),]
+    res = expression(dummy, 0)
+    print(res)
+    
+
+parse()
