e9f96b87641cd8417d9f8b89a007d49594d6b656
devi <utilities@evimay.mailer.me>
2026-04-07 18:16:25 +1000

    feat: enhance expression func to account for addition/substraction

--- Question 2/math_parser.py
+++ Question 2/math_parser.py
@@ -1,15 +1,32 @@
 def expression(token_list:list[tuple[str, str]], index:int) -> tuple[float, str, int]:
     """
-    Returns an expression value and next index.
+    Returns an expression value, tree and next index.
     """
+    # TODO: parentheses logic unimplemented
 
-    result, tree, last_index = factor(token_list, index)
+    value, tree, last_index = factor(token_list, index)
 
-    return result, tree, last_index
+    # consume all + and - tokens
+    while token_list[last_index][0] == "OP" and token_list[last_index][1] in ["+", "-"]:
+        operator = token_list[last_index][1]
+        right_factor, right_tree, last_index = factor(token_list, last_index + 1)
+
+        if operator == "+":
+            value += right_factor
+        else:
+            value -= right_factor
+        tree = "(" + operator + " " + tree + " " + right_tree + ")"
+    
+    # check for END token
+    if token_list[last_index][0] == "END":
+        return value, tree, last_index
+    
+    raise SyntaxError("Unexpected token: " + str(token_list[last_index]))
+
 
 def factor(token_list:list[tuple[str, str]], index:int) -> tuple[float, str, int]:
     """
-    Returns a factor value and next index.
+    Returns a factor value, tree and next index.
     """
     token = token_list[index]
 
@@ -29,10 +46,14 @@ def parse():
     Parses the input data and returns tree and result.
     """
 
-    dummy = [("OP", "-"),("OP", "-"),("NUM", "3"),("END", None)]
+    # --3 + 3 + -4
+    # dummy = [("OP", "-"), ("OP", "-"), ("NUM", "3"), ("OP", "+"), ("NUM", "3"), ("OP", "+"), ("OP", "-"), ("NUM", "4"), ("END", None)]
+    # dummy = [("OP", "-"),("OP", "-"),("NUM", "3"),("END", None)]
     # dummy = [("OP", "-"),("NUM", "3"),("END", None)]
     # dummy = [("NUM", "3"),("END", None)]
     # dummy = [("NUM", "0"),("END", None)]
+
+    dummy = [("OP", "-"), ("NUM", "10"), ("OP", "+"), ("NUM", "5"), ("OP", "-"), ("NUM", "2"), ("END", None)]
 
     value = None
     tree = None
