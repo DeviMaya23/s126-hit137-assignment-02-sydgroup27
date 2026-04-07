8263540f9bafb093a2fe398831b3ece478ccf45a
devi <utilities@evimay.mailer.me>
2026-04-07 14:31:00 +1000

    feat: add index in return value, raise SyntaxError for invalid tokens

--- Question 2/math_parser.py
+++ Question 2/math_parser.py
@@ -1,24 +1,28 @@
-def expression(token_list:list[tuple[str, str]], index:int):
+def expression(token_list:list[tuple[str, str]], index:int) -> tuple[float, int]:
     """
-    Returns an expression value.
+    Returns an expression value and next index.
     """
+
     result = factor(token_list, index)
+    if token_list[result[1]][0] != "END":
+        raise SyntaxError("Expect END token at index " + str(result[1]) + ", got " + str(token_list[result[1]]))
 
     return result
 
-def factor(token_list:list[tuple[str, str]], index:int):
+def factor(token_list:list[tuple[str, str]], index:int) -> tuple[float, int]:
     """
-    Returns a factor value.
+    Returns a factor value and next index.
     """
     token = token_list[index]
 
     if token[0] == "OP" and token[1] == "-":
-        return -factor(token_list, index + 1)
+        value, new_index = factor(token_list, index + 1)
+        return -value, new_index
 
     if token[0] == "NUM":
-        return float(token[1])
+        return float(token[1]), index + 1
     
-    return None
+    raise SyntaxError("Unexpected token: " + str(token))
 
 
 def parse():
@@ -26,9 +30,18 @@ def parse():
     Parses the input data and returns tree and result.
     """
 
-    dummy = [("OP", "-"),("OP", "-"),("NUM", "3"),]
-    res = expression(dummy, 0)
-    print(res)
+    dummy = [("OP", "+"),("OP", "-"),("NUM", "3"),("END", None)]
+    
+    value = None
+    try:
+        value = expression(dummy, 0)
+    except SyntaxError as e:
+        print("Syntax Error:", e)
+
+    if value is None:
+        print("No result")
+    else:
+        print("Result:", value)
     
 
 parse()
