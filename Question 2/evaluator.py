506328e1a00c43bee1f2b96e26ae724e83ac6db5
devi <utilities@evimay.mailer.me>
2026-04-11 12:46:47 +1000

    feat: init write result to output.txt

--- Question 2/evaluator.py
+++ Question 2/evaluator.py
@@ -19,8 +19,22 @@ def evaluate_file(input_path: str) -> list[dict]:
             "token": "dummytoken", # TODO: from tokenise function, maybe a new function to properly format
             "tree": "ERROR" if tree is None else tree, 
             "result": "ERROR" if result is None else result}) 
-        
-    # TODO: write result into output.txt, located in the same directory as input_path
+
+    # Write resuls to output.txt 
+    # TODO: output file location should match input file
+    with open("output.txt", "w") as f:
+        for result in results:
+            f.write(f"Input: {result['input']}\n")
+            f.write(f"Tree: {result['tree']}\n")
+            f.write(f"Tokens: {result['token']}\n")
+
+            result_value = result['result']
+            if result_value != "ERROR":
+                if result_value.is_integer():
+                    result_value = str(int(result_value))
+                else:
+                    result_value = f"{result_value:.4f}"
+            f.write(f"Result: {result_value}\n\n")
 
     return results
 
@@ -29,5 +43,7 @@ def dummy_tokenise():
     example1 = [("NUM", "3"), ("OP", "+"), ("NUM", "5"), ("END", None)]
     example2 = [("NUM", "3"), ("WRONG", "@"), ("NUM", "5"), ("END", None)]
     example3 = [("NUM", "3"), ("OP", "/"), ("NUM", "0"), ("END", None)]
-    return [example1, example2, example3]
+    example4 = [("NUM", "10"), ("OP", "/"), ("NUM", "3"), ("END", None)]
+    return [example1, example2, example3, example4]
 
+evaluate_file("dummy_input.txt")
