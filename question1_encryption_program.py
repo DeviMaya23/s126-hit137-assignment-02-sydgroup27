c7d53cc3e1b1d60f52ac385c797351a7936b5ddd
susmita sharma <ssusmita21@tbc.edu.np>
2026-04-14 14:11:06 +1000

    Created Question 1 encryption program files and raw input file

---
+++ question1_encryption_program.py
@@ -0,0 +1,22 @@
+# HIT137 Assignment 2 - Question 1
+# Encryption and Decryption Program
+
+
+def encrypt_file():
+    pass
+
+
+def decrypt_file():
+    pass
+
+
+def verify_files():
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
