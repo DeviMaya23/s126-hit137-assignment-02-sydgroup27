8294c0e566f4e36ea3f8e7108c4a6d34836e39c6
susmita sharma <ssusmita21@tbc.edu.np>
2026-04-14 15:35:17 +1000

    Added main program logic

--- question1_encryption_program.py
+++ question1_encryption_program.py
@@ -92,3 +92,21 @@ def decrypt_file(shift1, shift2):
         file.write(decrypted_text)
 
     print("Decryption completed successfully")
+# Main Program
+def main():
+
+    try:
+        shift1 = int(input("Enter shift1 value: "))
+        shift2 = int(input("Enter shift2 value: "))
+
+        encrypt_file(shift1, shift2)
+        decrypt_file(shift1, shift2)
+        verify_files()
+
+    except ValueError:
+        print("Please enter valid numbers")
+
+
+# Run program
+if __name__ == "__main__":
+    main()
