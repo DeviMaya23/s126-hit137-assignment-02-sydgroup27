5a208cbc1231355fd9628fe3fdbceb87994565bf
susmita sharma <ssusmita21@tbc.edu.np>
2026-04-14 15:02:00 +1000

    added Implement full character encryption function with conditional shift rules for A–Z and a–z

--- question1_encryption_program.py
+++ question1_encryption_program.py
@@ -1,5 +1,29 @@
 # HIT137 Assignment 2 - Question 1
 # Encryption and Decryption Program
+def encrypt_character(char, shift1, shift2):
+
+    if char.islower():
+
+        if 'a' <= char <= 'm':
+            shift = shift1 * shift2
+            return chr((ord(char) - ord('a') + shift) % 26 + ord('a'))
+
+        else:
+            shift = shift1 + shift2
+            return chr((ord(char) - ord('a') - shift) % 26 + ord('a'))
+
+    elif char.isupper():
+
+        if 'A' <= char <= 'M':
+            shift = shift1
+            return chr((ord(char) - ord('A') - shift) % 26 + ord('A'))
+
+        else:
+            shift = shift2 ** 2
+            return chr((ord(char) - ord('A') + shift) % 26 + ord('A'))
+
+    return char
+
 
 
 def encrypt_file():
